c1b01545ef3149a91dac0eb2a99e026a011d3700
righ <django.hamster@gmail.com>
2024-03-16 21:33:48 +0900

    publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
     globalObject: "this",
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: './',
   },
   plugins: [new TypescriptDeclarationPlugin({})],
   optimization: {
